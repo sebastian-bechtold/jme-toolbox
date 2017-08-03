89e730e66dbabdeab0fc1a5a19b372e5384b4b28
sbecht <sbecht>
2017-08-03 20:25:18 +0200

    Improved mouse object dragging

--- src/com/sebastianbechtold/jmeToolbox/manfredModules/MouseDragSystem.java
+++ src/com/sebastianbechtold/jmeToolbox/manfredModules/MouseDragSystem.java
@@ -4,6 +4,9 @@ import com.jme3.app.Application;
 import com.jme3.app.state.AppStateManager;
 import com.jme3.collision.CollisionResult;
 import com.jme3.collision.CollisionResults;
+import com.jme3.input.MouseInput;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.MouseAxisTrigger;
 import com.jme3.math.Ray;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
@@ -35,6 +38,65 @@ public class MouseDragSystem extends AbstractManfredJmeAppState {
 
 	}
 
+	AnalogListener mAnalogListener = new AnalogListener() {
+
+		@Override
+		public void onAnalog(String name, float value, float tpf) {
+
+			if ("RotateX".equals(name) || "RotateY".equals(name)) {
+
+				if (mDraggedEntity == -1) {
+					return;
+				}
+
+				Vec3PosCmp pc = mEm.getComponent(mDraggedEntity, Vec3PosCmp.class);
+
+				if (pc == null) {
+					return;
+				}
+
+				Camera cam = mCamera;
+
+				Vector2f shiftedMouseCoords = new Vector2f(mInputManager.getCursorPosition()).add(mDiff);
+
+				Vector3f mouseRayOrigin = cam.getWorldCoordinates(shiftedMouseCoords, 0);
+
+				Vector3f mouseRayDir = cam.getWorldCoordinates(shiftedMouseCoords, 1).subtractLocal(cam.getWorldCoordinates(shiftedMouseCoords, 0))
+						.normalizeLocal();
+
+				Ray baseRay = new Ray(mouseRayOrigin, mouseRayDir);
+
+				CollisionResults results = new CollisionResults();
+
+				mTerrainNode.collideWith(baseRay, results);
+
+				CollisionResult closest = results.getClosestCollision();
+
+				if (closest != null) {
+
+					Vector3f newObjectPos = closest.getContactPoint();
+
+					// Update PositionComponent:
+					mEm.setComponent(mDraggedEntity, new Vec3PosCmp(StaticUtils.jme2am_vector(newObjectPos)));
+
+					// ############## BEGIN Update 2D drag offset #################
+					// Vector3f objectScreenPos3D = mCamera.getScreenCoordinates(newObjectPos);
+					// Vector2f objectScreenPos2D = new Vector2f(objectScreenPos3D.getX(), objectScreenPos3D.getY());
+
+					Vector2f objectScreenPos2D = shiftedMouseCoords;
+
+					Vector3f grabScreenPos3d = mCamera.getScreenCoordinates(newObjectPos.add(mCenterToGrabOffset));
+					Vector2f grabScreenPos2d = new Vector2f(grabScreenPos3d.getX(), grabScreenPos3d.getY());
+
+					mDiff = objectScreenPos2D.subtract(grabScreenPos2d);
+					// ############## END Update 2D drag offset #################
+
+				}
+
+			}
+		}
+	};
+
 	@Override
 	public void cleanup() {
 
@@ -49,55 +111,12 @@ public class MouseDragSystem extends AbstractManfredJmeAppState {
 
 		Easyvents.defaultDispatcher.addListener(ManfredJmeSpatialCompClickedRight.class, this::onComponentRightClicked);
 		Easyvents.defaultDispatcher.addListener(JmeMouseButtonReleasedRight.class, this::onRmbUp);
-	}
 
-	@Override
-	public void update(float tpf) {
+		mInputManager.addMapping("RotateX", new MouseAxisTrigger(MouseInput.AXIS_X, false), new MouseAxisTrigger(MouseInput.AXIS_X, true));
 
-		if (mDraggedEntity == -1) {
-			return;
-		}
+		mInputManager.addMapping("RotateY", new MouseAxisTrigger(MouseInput.AXIS_Y, false), new MouseAxisTrigger(MouseInput.AXIS_Y, true));
 
-		Vec3PosCmp pc = mEm.getComponent(mDraggedEntity, Vec3PosCmp.class);
-
-		if (pc == null) {
-			return;
-		}
-
-		Camera cam = mCamera;
-
-		Vector2f shiftedMouseCoords = new Vector2f(mInputManager.getCursorPosition()).add(mDiff);
-
-		Vector3f mouseRayOrigin = cam.getWorldCoordinates(shiftedMouseCoords, 0);
-
-		Vector3f mouseRayDir = cam.getWorldCoordinates(shiftedMouseCoords, 1).subtractLocal(cam.getWorldCoordinates(shiftedMouseCoords, 0)).normalizeLocal();
-
-		Ray baseRay = new Ray(mouseRayOrigin, mouseRayDir);
-
-		CollisionResults results = new CollisionResults();
-
-		mTerrainNode.collideWith(baseRay, results);
-
-		CollisionResult closest = results.getClosestCollision();
-
-		if (closest != null) {
-
-			Vector3f hitPoint = closest.getContactPoint();
-
-			// Update PositionComponent:
-			mEm.setComponent(mDraggedEntity, new Vec3PosCmp(StaticUtils.jme2am_vector(hitPoint)));
-
-			//############## BEGIN Update 2D drag offset #################
-			Vector3f foo = mCamera.getScreenCoordinates(hitPoint);
-			Vector2f obsc = new Vector2f(foo.getX(), foo.getY());
-
-			Vector3f grabScreenPos = mCamera.getScreenCoordinates(hitPoint.add(mCenterToGrabOffset));
-
-			Vector2f grabScreenPos2d = new Vector2f(grabScreenPos.getX(), grabScreenPos.getY());
-
-			mDiff = obsc.subtract(grabScreenPos2d);
-			//############## END Update 2D drag offset #################
-		}
+		mInputManager.addListener(mAnalogListener, "RotateX", "RotateY");
 
 	}
 
@@ -109,9 +128,6 @@ public class MouseDragSystem extends AbstractManfredJmeAppState {
 
 		Vector3f rayOrigin = cam.getWorldCoordinates(mouseCoords, 0);
 		Vector3f rayDir = cam.getWorldCoordinates(mouseCoords, 1).subtractLocal(cam.getWorldCoordinates(mouseCoords, 0)).normalizeLocal();
-
-		// Ray ray = new Ray( cam.getWorldCoordinates(mouseCoords, 0), cam.getWorldCoordinates(mouseCoords,
-		// 1).subtractLocal(cam.getWorldCoordinates(mouseCoords, 0)).normalizeLocal());
 
 		Ray ray = new Ray(rayOrigin, rayDir);
 
@@ -130,6 +146,7 @@ public class MouseDragSystem extends AbstractManfredJmeAppState {
 	public void onComponentRightClicked(Object payload) {
 
 		JmeNodeComponentMouseEvent event = (JmeNodeComponentMouseEvent) payload;
+		// TODO 2: Include collision results in JmeNodeComponentMouseEvent
 		int id = event.mEntityId;
 
 		if (id == -1) {
@@ -143,29 +160,23 @@ public class MouseDragSystem extends AbstractManfredJmeAppState {
 			return;
 		}
 
-		CollisionResults results_terrain = mousePick(mTerrainNode);
-		CollisionResult closest_terrain = results_terrain.getClosestCollision();
-
 		CollisionResults results_obj = mousePick(event.mSpatial);
 		CollisionResult closest_obj = results_obj.getClosestCollision();
 
-		if (closest_terrain != null) {
+		if (closest_obj != null) {
 
 			Vector3f contact_obj = closest_obj.getContactPoint();
-
 			Vector3f objCenter = StaticUtils.am2jme_vector(pc.getPos());
 
 			mCenterToGrabOffset = contact_obj.subtract(objCenter);
 
-			Vector3f foo = mCamera.getScreenCoordinates(objCenter);
-			Vector2f obsc = new Vector2f(foo.getX(), foo.getY());
+			Vector3f grabScreenPos3D = mCamera.getScreenCoordinates(contact_obj);
+			Vector2f grabScreenPos2d = new Vector2f(grabScreenPos3D.getX(), grabScreenPos3D.getY());
 
-			Vector3f grabScreenPos = mCamera.getScreenCoordinates(objCenter.add(mCenterToGrabOffset));
+			Vector3f objectScreenPos3D = mCamera.getScreenCoordinates(objCenter);
+			Vector2f objectScreenPos2D = new Vector2f(objectScreenPos3D.getX(), objectScreenPos3D.getY());
 
-			Vector2f grabScreenPos2d = new Vector2f(grabScreenPos.getX(), grabScreenPos.getY());
-
-			mDiff = obsc.subtract(grabScreenPos2d);
-
+			mDiff = objectScreenPos2D.subtract(grabScreenPos2d);
 		}
 
 		mDraggedEntity = id;
